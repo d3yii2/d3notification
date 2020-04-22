faafe695919c8e5889c1e2966ccd784cbf9c0f30
uldisn <uldisnelsons@gmail.com>
2020-04-22 08:14:44 +0300

    Added files

---
+++ models/D3nType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace d3yii2\d3notification\models;
+
+use \d3yii2\d3notification\models\base\D3nType as BaseD3nType;
+
+/**
+ * This is the model class for table "d3n_type".
+ */
+class D3nType extends BaseD3nType
+{
+
+}
